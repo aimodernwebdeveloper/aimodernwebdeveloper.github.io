8bfc197f3d5c3ee05cf6a0b12834ca031fdfaa15
aimodernwebdeveloper <69334764+aimodernwebdeveloper@users.noreply.github.com>
2021-08-18 21:45:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 function HelloWorld() {
-     <h2>Hello world from React!</h2>
+     return <h2>Hello world from React!</h2>;
 }
 
 ReactDom.render(<HelloWorld />, document.getElementById('root'));
